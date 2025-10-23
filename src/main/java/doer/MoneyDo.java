90be21fc3ce05e57231b1ea858eadac097943250
Jakob <jakob.makela@ha.ax>
2025-10-23 11:21:52 +0300

    MoneyDoTest.java dollarToEuroConversionTest 2 red again

--- src/main/java/doer/MoneyDo.java
+++ src/main/java/doer/MoneyDo.java
@@ -1,11 +1,13 @@
 package doer;
 
 import java.math.BigDecimal;
+import java.math.MathContext;
+import java.math.RoundingMode;
 
 public class MoneyDo {
     private static final BigDecimal USD_TO_EUR = new BigDecimal("92.00");
 
     public BigDecimal dollarsToEuros(BigDecimal dollarAmount) {
-        return new BigDecimal("92.00");
+        return dollarAmount.multiply(USD_TO_EUR, new MathContext(2, RoundingMode.HALF_UP));
     }
 }
