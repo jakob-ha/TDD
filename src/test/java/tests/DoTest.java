e06ce380e84b11465b1214ce267615e2e89f698a
Jakob <jakob.makela@ha.ax>
2025-10-23 08:36:09 +0300

    removed unparameterized tests

--- src/test/java/tests/DoTest.java
+++ src/test/java/tests/DoTest.java
@@ -22,27 +22,6 @@ public class DoTest {
         does.injectName("Injected Name");
     }
 
-    @Test
-    @DisplayName("Add positive")
-    public void addTest() {
-        int result = does.add(2,3);
-        Assertions.assertEquals(5, result);
-    }
-
-    @Test
-    @DisplayName("Add zero")
-    public void addZeroTest() {
-        int result = does.add(5,0);
-        Assertions.assertEquals(5, result);
-    }
-
-    @Test
-    @DisplayName("Add negative")
-    public void addNegativeTest() {
-        int result = does.add(-2,-3);
-        Assertions.assertEquals(-5, result);
-    }
-
     @ParameterizedTest
     @MethodSource("sumTestData")
     @DisplayName("Addstests")
@@ -58,27 +37,5 @@ public class DoTest {
                 Arguments.of(-4,-3,-7)
         );
     }
-
-
-    @Test
-    public void subTest() {
-        int result = does.sub(5,3);
-        Assertions.assertEquals(2, result);
-    }
-
-/*
-    @Test
-    public void mulTest() {
-        int result = does.mul(3,2);
-        Assertions.assertEquals(6, result);
-    }
-
-    @Test
-    public void divTest() {
-        int result = does.div(6,3);
-        Assertions.assertEquals(2, result);
-    }
-    */
-
 
 }
