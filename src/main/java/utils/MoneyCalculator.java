539ca73dbd5da4bb5f14bcdd89290ded301c7b91
Jakob <jakob.makela@ha.ax>
2025-10-28 08:30:48 +0200

    shouldThrowExceptionForNegativeUSD test GREEN

--- src/main/java/utils/MoneyCalculator.java
+++ src/main/java/utils/MoneyCalculator.java
@@ -8,6 +8,7 @@ public class MoneyCalculator {
     private static final BigDecimal USD_TO_EUR = new BigDecimal("0.92");
 
     public BigDecimal dollarsToEuros(BigDecimal dollarAmount) {
+        if (dollarAmount.signum() == -1) {throw new IllegalArgumentException("dollarAmount must not be negative");}
         return dollarAmount.multiply(USD_TO_EUR, new MathContext(3, RoundingMode.HALF_UP));
     }
 }
