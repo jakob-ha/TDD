27409fb88c8c75aecbfe619636778ef89b3d87c7
Jakob <jakob.makela@ha.ax>
2025-10-28 08:23:42 +0200

    MoneyCalculatorTest.java removed invalid test parameters dollarToEuroConversionTest added shouldThrowExceptionForNegativeUSD test RED

--- src/test/java/tests/MoneyCalculatorTest.java
+++ src/test/java/tests/MoneyCalculatorTest.java
@@ -1,5 +1,6 @@
 package tests;
 
+import org.junit.jupiter.api.Test;
 import utils.MoneyCalculator;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -9,6 +10,7 @@ import org.junit.jupiter.params.provider.CsvSource;
 import java.math.BigDecimal;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 
 public class MoneyCalculatorTest {
@@ -24,11 +26,15 @@ public class MoneyCalculatorTest {
     @CsvSource({
             "100.00, 92.00",
             "200.00, 184.00",
-            "0.00,0.00",
-            "-200.00, -184.00"})
+            "0.00,0.00"})
     @DisplayName("Dollar to Euro Converter")
     void dollarToEuroConversionTest(BigDecimal dollarAmount, BigDecimal euroAmountExpected) {
         BigDecimal euroValueCalculated = moneyCalculator.dollarsToEuros(dollarAmount);
         assertThat(euroValueCalculated).isEqualByComparingTo(euroAmountExpected);
+    }
+
+    @Test
+    void shouldThrowExceptionForNegativeUSD() {
+        assertThatThrownBy(() -> moneyCalculator.dollarsToEuros(new BigDecimal("-10.00"))).isInstanceOf(IllegalArgumentException.class).hasMessageContaining("negative");
     }
 }
