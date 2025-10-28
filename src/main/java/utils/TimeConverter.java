bd5833530aeadf54233a439d76919c056c201691
Jakob <jakob.makela@ha.ax>
2025-10-28 10:24:21 +0200

    shouldConvert60SecondsTo1Minute() hard coded GREEN

---
+++ src/main/java/utils/TimeConverter.java
@@ -0,0 +1,10 @@
+package utils;
+
+public class TimeConverter {
+    private static final int SECONDS_PER_MINUTE = 60;
+
+    public double secondsToMinutes(long seconds) {
+        return 1.0;
+        //return (double) seconds / SECONDS_PER_MINUTE;
+    }
+}
--- src/test/java/tests/TimeConverterTest.java
+++ src/test/java/tests/TimeConverterTest.java
@@ -2,10 +2,12 @@ package tests;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import utils.TimeConverter;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class TimeConverterTest {
+    private final TimeConverter converter = new TimeConverter();
 
     @Test
     @DisplayName("Should convert 60 seconds to 1 minute")
