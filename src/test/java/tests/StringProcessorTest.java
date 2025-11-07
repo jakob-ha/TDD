9b2eaddbae1aae87e8c4ede9e70eb742c139f397
Jakob <jakob.makela@ha.ax>
2025-11-07 08:38:41 +0200

    shouldReverseSimpleString contains spaces GREEN

--- src/test/java/tests/StringProcessorTest.java
+++ src/test/java/tests/StringProcessorTest.java
@@ -22,7 +22,8 @@ public class StringProcessorTest {
     @ParameterizedTest
     @CsvSource({"abra, arba",
     "kadabra, arbadak",
-    "'',''"})
+    "'',''",
+    "this contains spaces,secaps sniatnoc siht"})
     @DisplayName("Tests whether simple strings can be reversed")
     public void shouldReverseSimpleString(String input, String expected) {
         assertEquals(expected, processor.reverse(input));
